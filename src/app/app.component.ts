dec9c72e1a4be5520b58ae13da106b1ab2d0a3b2
Allen <alenachankunju@gmail.com>
2021-07-10 22:53:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project';
+  // Username: string = 'Enter username';
+
+  userlist: any = [
+    { Id: '1',Name: 'Manu', Age: '24', Email: 'manu@gmail.com' },
+    { Id: '2',Name: 'Sanu', Age: '19', Email: 'sanu@gmail.com' },
+    { Id: '3',Name: 'Sabu', Age: '39', Email: 'sabu@gmail.com' },
+    { Id: '4',Name: 'Jithin', Age: '27', Email: 'jithin@gmail.com' },
+    { Id: '5',Name: 'Rakesh', Age: '10', Email: 'rakesh@gmail.com' },
+    { Id: '6',Name: 'Anu', Age: '30', Email: 'anu@gmail.com' },
+  ];
+
+  // onclick(prouser: { value: any }) {
+  //   if (prouser.value.length > 0) {
+  //     this.userlist.push(prouser.value);
+  //     prouser.value = '';
+  //   }
+  // }
+
+  ondelete(id:number) {
+    // const foundIndex = this.userlist.value.findIndex(id === this.id);
+    this.userlist.splice(id,1);
+  }
 }
